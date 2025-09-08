4c248e3f79d889a4392240638e1023e7d7705d9a
Lucas Merckelbach <lucas.merckelbach@hereon.de>
2025-09-08 11:12:59 +0200

    Fixes test.

--- examples/quick_example.py
+++ examples/quick_example.py
@@ -3,7 +3,8 @@
 from argos import argosClient
 
 api = argosClient.ArgosPlatformInfo(credentials="/home/lucas/.local/share/argos/argos_login.txt")
-api.retrieve(platformId=260603, number_of_days_from_now=20)
+#api.retrieve(platformId=260603, number_of_days_from_now=20)
+api.retrieve(platformId=27011, number_of_days_from_now=20)
 info = api.get_info(latest_only=True)
 payload = info['payload']
 print(payload)
